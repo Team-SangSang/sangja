33adebba380c3760b0b545dbd07f1e93a585c731
Qwaz <qwazpia@gmail.com>
2015-01-28 13:52:51 +0900

    Implement Union name input

--- blockbuilder/select-object.js
+++ blockbuilder/select-object.js
@@ -3,30 +3,41 @@
 (function () {
     "use strict";
     
-    var SELECTION_OUTLINE_COLOR = 0x00FF00,
+    var SELECTION_BLOCK_OUTLINE = 0x00FF00,
+        SELECTION_UNION_OUTLINE = 0x0000FF,
         SELECT_NONE_ID = 'select-none',
         SELECTED_ID = 'select-selected',
         SELECT_UNION_ID = 'select-union',
         UNION_BUTTON_ID = 'select-create-union',
+        UNION_NAME_INPUT = 'select-union-name',
         
         selectedObjects = [];
     
     function displayMenu() {
         var selectNone = $('#' + SELECT_NONE_ID),
             selected = $('#' + SELECTED_ID),
-            selectUnion = $('#' + SELECT_UNION_ID);
+            selectUnion = $('#' + SELECT_UNION_ID),
+            
+            unionNameInput = $('#' + UNION_NAME_INPUT);
         
         $('#toolmenu-select > div').css('display', 'none');
         
         if (selectedObjects.length === 0) {
+            //아무것도 선택 안 됨
             selectNone.css('display', '');
         } else if (selectedObjects.length === 1 && selectedObjects[0] instanceof BlockBuilder.Union) {
+            //유니온 하나 선택
             selectUnion.css('display', '');
-            //TODO Union 관련 처리
+            unionNameInput.val(selectedObjects[0].name);
         } else {
+            //다중 오브젝트 선택
             selected.css('display', '');
             selected.children('p').text(selectedObjects.length + ' objects selected');
         }
+    }
+    
+    function inputUnionName() {
+        selectedObjects[0].name = $('#' + UNION_NAME_INPUT).val();
     }
     
     function deselectAll() {
@@ -46,7 +57,7 @@
         blockIndex = selectedObjects.indexOf(object);
         
         if (blockIndex === -1) {
-            object.showGuideBox(SELECTION_OUTLINE_COLOR);
+            object.showGuideBox(object instanceof BlockBuilder.Block ? SELECTION_BLOCK_OUTLINE : SELECTION_UNION_OUTLINE);
             object.setOpacity(0.8);
             
             selectedObjects.push(object);
@@ -109,5 +120,7 @@
             displayMenu();
             BlockBuilder.render();
         });
+        
+        $('#' + UNION_NAME_INPUT).on('input', inputUnionName);
     });
 }());
